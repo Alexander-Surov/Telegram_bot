2db442a019911ec3c11655bf48f66de43efbe923
Alexander-Surov <115669356+Alexander-Surov@users.noreply.github.com>
2023-03-07 04:56:10 +0300

    Create cryptographer.py
    Только объявления классов

---
+++ cryptographer.py
@@ -0,0 +1,21 @@
+from utils import ConvertToNumbers
+
+
+class ICipher():
+    def __init__(self):
+        pass
+
+
+class Caesar(ICipher):
+    def __init__(self):
+        pass
+
+
+class Visener(ICipher):
+    def __init__(self):
+        pass
+
+
+class Vernam(ICipher):
+    def __init__(self):
+        pass
