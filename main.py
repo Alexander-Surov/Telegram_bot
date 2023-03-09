1ab67cff9a27a3f256c1fc7c3004c7a440aa5d81
Alexander-Surov <115669356+Alexander-Surov@users.noreply.github.com>
2023-03-09 23:13:12 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from api import telegram_bot_cryptographer as tg_bot
+from pathlib import Path
+
+
+token = Path("Telegram Bot/Token.txt").read_text()
+
+language_path = "Telegram Bot/languages.csv"
+rate_path = None
+
+
+print("\n◢ Telegram_bot was started ◣\n")
+
+tg_bot.TelegramBot(token, language_path, rate_path).run()
+
+print("\n◥ Telegram_bot was stopped ◤\n")
